e9240a46bfdde0b49a73ade048aff91f50a98097
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-21 10:41:48 -0300

    :sparkles: feat: Adicionado page Perfil

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import DeletarTema from './components/temas/deletartema/DeletarTema';
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagens';
 import FormPostagem from './components/postagens/formpostagem/FormPostagem';
 import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem';
+import Perfil from './pages/perfil/Perfil';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
               <Route path="/postagens" element={<ListaPostagens />} />
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
               <Route
                 path="/deletarpostagem/:id"
                 element={<DeletarPostagem />}
